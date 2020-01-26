1b378d9d2f96b974f06cf595fdbd05883d95cc3c
Nirav Patel <niravsavaliya1122@gmail.com>
2020-01-25 20:55:55 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/simplenoticeapp/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.simplenoticeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,StartActivity.class));
+                finish();
+            }
+        },2500);
+
+    }
+}
